78bab6d3ff2a9f46df24f17ff52234b00c90bd3c
Vanesa <57409926+vanesa33@users.noreply.github.com>
2021-02-08 15:44:41 -0300

    operadores arimeticos operadores arimeticos

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace operadores_aritmes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //operadores aritmeticos
+
+            double num, pot, resultado;
+
+            Console.WriteLine("Digite el numero que quiere elevar: ");
+            num = NewMethod();
+
+            Console.WriteLine("digite a la potencia que quiera elevar: ");
+            pot = Convert.ToDouble(Console.ReadLine());
+
+            resultado = Math.Pow(num, pot);
+            Console.WriteLine("el resultado es: "+ resultado);
+            Console.ReadKey();
+
+
+        }
+
+        private static double NewMethod()
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
